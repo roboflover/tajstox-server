7d7aa48175fbc0729639c0cdfef43425e1816fe2
Your Name <you@example.com>
2024-11-16 01:04:02 +0300

    privet

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,21 +7,35 @@ export class AuthService {
 
   verifyTelegramData(initData: string): boolean {
     console.log('Verifying Telegram data...');
-    const secretKey = crypto.createHash('sha256').update(process.env.BOT_TOKEN).digest();
-    const parsedData = new URLSearchParams(initData);
-    const hash = parsedData.get('hash');
+
+    // The data is a query string, which is composed of a series of field-value pairs.
+    const encoded = decodeURIComponent(initData); 
     
-    // Создаем строку данных для проверки
-    const dataCheckString = [...parsedData.entries()]
-      .filter(([key]) => key !== 'hash')
-      .map(([key, value]) => `${key}=${value}`)
-      .sort() // Параметры должны быть отсортированы по алфавиту
-      .join('\n'); // Используем правильный символ новой строки
-  
-    // Вычисляем хеш
-    const hmac = crypto.createHmac('sha256', secretKey).update(dataCheckString).digest('hex');
-    console.log(`Expected hash: ${hmac}, Received hash: ${hash}`);
-    return hmac === hash;
+    
+    // HMAC-SHA-256 signature of the bot's token with the constant string WebAppData used as a key.
+    const secret = crypto
+      .createHmac('sha256', 'WebAppData')
+      .update(process.env.BOT_TOKEN);
+
+    // Data-check-string is a chain of all received fields'.
+    const arr = encoded.split('&');
+    const hashIndex = arr.findIndex(str => str.startsWith('hash='));
+    const hash = arr.splice(hashIndex)[0].split('=')[1];
+    // sorted alphabetically
+    arr.sort((a, b) => a.localeCompare(b));
+    // in the format key=<value> with a line feed character ('\n', 0x0A) used as separator
+    // e.g., 'auth_date=<auth_date>\nquery_id=<query_id>\nuser=<user>
+    const dataCheckString = arr.join('\n');
+    
+    // The hexadecimal representation of the HMAC-SHA-256 signature of the data-check-string with the secret key
+    const _hash = crypto
+      .createHmac('sha256', secret.digest())
+      .update(dataCheckString)
+      .digest('hex');
+    
+    // if hash are equal the data may be used on your server.
+    // Complex data types are represented as JSON-serialized objects.
+    return _hash === hash;
   }
 
   getUserId(initData: string): number {
