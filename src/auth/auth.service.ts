7ff024cefa92e91095c6f28f80736e9ecd4bae8a
Your Name <you@example.com>
2024-11-17 22:13:27 +0300

    ooo

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,7 +6,8 @@ export class AuthService {
   private readonly logger = new Logger(AuthService.name);
 
   validateInitData(authData: string): InitData {
-    const token = '8193856623:AAHvmJCbFTkaxVSxJ4ooq0Q-LmQLvH3Va3Q';
+    //const token = '8193856623:AAHvmJCbFTkaxVSxJ4ooq0Q-LmQLvH3Va3Q';
+    const token = '7616166878:AAHKr7IvCJz7hSPZTi3lj9vYQ5j7JAiWTOw' //cosmotap
 
     this.logger.debug(`Received authData: ${authData}`);
 
