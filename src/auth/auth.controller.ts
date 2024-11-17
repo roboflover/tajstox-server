eb4cf4090a2cc31c0806cb3b9891ba71e34eb37c
Your Name <you@example.com>
2024-11-17 21:15:50 +0300

    ooo

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,30 +1,37 @@
 // auth.controller.ts
 
-import { Controller, Post, UnauthorizedException, Headers, Get } from '@nestjs/common';
+import { Controller, Post, UnauthorizedException, Headers, Get, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PublicRoute } from "../decorators/public-route.decorator"
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('authenticate')
-  authenticateUser(@Headers('authorization') authorization: string): string {
-    const [authType, initData] = (authorization || '').split(' ');
-    // console.log('initData', initData)
-    if (authType !== 'tma' || !initData) {
-      console.log('Неверная схема авторизации')
-      throw new UnauthorizedException('Неверная схема авторизации');
+    @PublicRoute()
+    @Post("login")
+    async login(@Body() body: { initData: string }) {
+        return await this.authService.validateInitData(body.initData)
     }
 
-    try {
-      console.log('вызываю метод validateInitData')
-      // Используем existing метод validateInitData для валидации
-      const validData = this.authService.validateInitData(initData);
-      // Например, можем вернуть часть валидных данных
-      return `Authentication successful: ${JSON.stringify(validData)}`;
-    } catch (error) {
-      throw new UnauthorizedException('Ошибка аутентификации пользователя');
-    }
-  }
+  // @Post('authenticate')
+  // authenticateUser(@Headers('authorization') authorization: string): string {
+  //   const [authType, initData] = (authorization || '').split(' ');
+  //   // console.log('initData', initData)
+  //   if (authType !== 'tma' || !initData) {
+  //     console.log('Неверная схема авторизации')
+  //     throw new UnauthorizedException('Неверная схема авторизации');
+  //   }
+
+  //   try {
+  //     console.log('вызываю метод validateInitData')
+  //     // Используем existing метод validateInitData для валидации
+  //     const validData = this.authService.validateInitData(initData);
+  //     // Например, можем вернуть часть валидных данных
+  //     return `Authentication successful: ${JSON.stringify(validData)}`;
+  //   } catch (error) {
+  //     throw new UnauthorizedException('Ошибка аутентификации пользователя');
+  //   }
+  // }
 
 }
