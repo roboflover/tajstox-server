18f1a16ed6c2d752712c9138225922fa63d3288d
Your Name <you@example.com>
2024-11-17 21:17:55 +0300

    ooo

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,7 +9,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
     @PublicRoute()
-    @Post("login")
+    @Post("authorization")
     async login(@Body() body: { initData: string }) {
         return await this.authService.validateInitData(body.initData)
     }
