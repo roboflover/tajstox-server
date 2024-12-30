898cd4c797a068e3376a9ef69ecb75751a5249d6
Your Name <you@example.com>
2024-12-30 04:54:28 +0300

    ppp

--- src/progress/progress.service.ts
+++ src/progress/progress.service.ts
@@ -14,16 +14,13 @@ export class ProgressService {
     const now = new Date();
 
     if (!userProgress) {
-      // Если запись прогресса для пользователя не существует, создаём новую
       userProgress = await this.prisma.userProgress.create({
         data: { telegramId, lastInteraction: now, currentStreak: 1 },
       });
       return userProgress.currentStreak;
     }
 
-    // Проверяем, прошло ли больше суток с момента последнего действия
     if (isAfter(now, addDays(userProgress.lastInteraction, 1))) {
-      // Сбрасываем последовательность дней
       userProgress = await this.prisma.userProgress.update({
         where: { telegramId },
         data: { currentStreak: 1, lastInteraction: now },
@@ -34,49 +31,49 @@ export class ProgressService {
   }
 
   async updateDay(telegramId: string, day: number, bonus: number): Promise<number> {
-    console.log(telegramId, day, bonus)
-    // Получаем информацию о прогрессе пользователя
+    const now = new Date();
+    
     let userProgress = await this.prisma.userProgress.findUnique({
       where: { telegramId },
     });
-  
+
     if (!userProgress) {
       throw new Error('User progress not found');
     }
-  
-    // Проверяем, что отправленный день совпадает с текущей длиной цепочки
+
+    // Ensure the action is performed only once per 24 hours period
+    if (!isAfter(now, addDays(userProgress.lastInteraction, 1))) {
+      throw new Error('Action can only be performed once every 24 hours');
+    }
+
     if (userProgress.currentStreak !== day) {
       throw new Error('Invalid day');
     }
-  
-    // Находим пользователя по telegramId
+
     const user = await this.prisma.user.findUnique({
       where: { telegramId },
     });
-  
+
     if (!user) {
       throw new Error('User not found');
     }
-  
-    // Обновляем score пользователя, добавляя к нему бонус
+
     await this.prisma.user.update({
       where: { telegramId },
       data: {
         score: user.score + bonus,
       },
     });
-  
-    // Обновляем день и дату взаимодействия
-    const nextDay = day >= 15 ? 1 : day + 1; // Если день больше 15, сбрасываем на 1
+
+    const nextDay = day >= 15 ? 1 : day + 1;
     userProgress = await this.prisma.userProgress.update({
       where: { telegramId },
       data: {
         currentStreak: nextDay,
-        lastInteraction: new Date(),
+        lastInteraction: now,
       },
     });
-  
+
     return userProgress.currentStreak;
   }
-  
 }
